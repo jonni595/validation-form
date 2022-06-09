a5a18222af5142096f3407dc7dd32fedf759e506
jonathan <jonathandajome@gmail.com>
2022-06-09 17:02:32 -0500

    se eliminaron los archivos por defecto de creacte App, y se generó el archivo base de los estilos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  return(
+    <main>
+      <form action=''>
+        <label htmlFor=''>Usuario</label>
+        <input type='text' placeholder='usuario'/>
+        <p>lorem ipsum</p>
+      </form>
+    </main>
   );
 }
 
