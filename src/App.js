284ac27c00f1b608333c25cfa2d7f345f1fe1fa6
jonathan <jonathandajome@gmail.com>
2022-06-09 21:40:08 -0500

    creamos el componente Input

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React from 'react';
-import { Formulario, Label, GrupoInput, Input } from "./elements/forms";
-import './styles.css';
+import { Formulario, Label, ContenedorTerminos, ContenedorBotonCentrado, Boton, MensajeExito, MensajeError } from "./elements/forms";
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+import './styles.css';
+import ComponenteInput from './components/input';
 
 const App = () => {
   return(
     <main>
       <Formulario action=''>
-        <div>
-          <Label htmlFor=''>Usuario</Label>
-          <GrupoInput>
-            <Input type='text' placeholder='usuario'/>
-            <FontAwesomeIcon icon={faCircleCheck}/>
-          </GrupoInput>
-            <p>lorem ipsum</p>
-        </div>
+        <ComponenteInput
+          label="Usuario"
+          placeholder="tathan522"
+          name="usuario"
+          leyendaError="El usuario tiene que ser de 4 a 16 digitos, y solo puede contener numeros, letras y guion bajo."
+          expresionRegular=""
+        />
+        <ComponenteInput
+          label="Password"
+          placeholder="Escribe tu contraseña"
+          name="password"
+          leyendaError="El usuario tiene que ser de 4 a 16 digitos, y solo puede contener numeros, letras y guion bajo."
+          expresionRegular=""
+        />
+
+        <ContenedorTerminos>
+          <Label>
+              <input type="checkbox" name="terminos" id="terminos"/>
+              Acepto los Términos y Condiciones
+          </Label>
+        </ContenedorTerminos>
+
+        {false && <MensajeError>
+          <p>
+          <FontAwesomeIcon icon={faExclamationTriangle}/>
+          <b>Error:</b>P
+          or favor rellena el formulario correctamente.</p>
+        </MensajeError>}
+
+        <ContenedorBotonCentrado>
+          <Boton type="submit">Enviar</Boton>
+          <MensajeExito>Formulario enviado exitosamente!!</MensajeExito>
+        </ContenedorBotonCentrado>
+
       </Formulario>
     </main>
   );
--- src/elements/forms.js
+++ src/elements/forms.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const colors = {
     border: "#37E2D5",
     error: "#D61C4E",
-    successful: "#2F8F9D"
+    successful: "#00FFAB"
 };
 
 const Formulario = styled.form`
@@ -48,4 +49,85 @@ const Input = styled.input`
     }
 `;
 
-export { Formulario, Label, GrupoInput, Input };
+const LeyendaError = styled.p`
+    font-size: 12px;
+    margin-bottom: 0;
+    color: ${colors.error};
+    display: none;
+`;
+
+const IconoValidacion = styled(FontAwesomeIcon)`
+    position: absolute;
+    right: 10px;
+    bottom: 14px;
+    z-index: 100;
+    font-size: 16px;
+    opacity: 0;
+`;
+
+const ContenedorTerminos = styled.div`
+    grid-column: span 2;
+    input {
+        margin-right: 10px;
+    }
+`;
+
+const ContenedorBotonCentrado = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    grid-column: span 2;
+`;
+
+const Boton = styled.button`
+    height: 45px;
+    line-height: 45px;
+    width: 30%;
+    background: #2FA4FF;
+    color: #fff;
+    font-weight: bold;
+    border: none;
+    border-radius: 3px;
+    cursor: pointer;
+    transition: .1s ease all;
+
+    &:hover{
+        box-shadow: 3px 0px 15px rgb(94,230,255, 1);
+    }
+`;
+
+const MensajeExito = styled.p`
+    font-size: 14px;
+    color: ${colors.successful};
+    display: none;
+`;
+
+const MensajeError = styled.div`
+    height: 45px;
+    line-height: 45px;
+    background: ${colors.error};
+    padding: 0 15px;
+    border-radius: 3px;
+    grid-column: span 2;
+
+    p{
+        margin: 0;
+    }
+    b {
+        margin-left: 10px;
+    }
+`;
+
+export {
+    Formulario,
+    Label,
+    GrupoInput,
+    Input,
+    LeyendaError,
+    IconoValidacion,
+    ContenedorTerminos,
+    ContenedorBotonCentrado,
+    Boton,
+    MensajeExito,
+    MensajeError
+};
