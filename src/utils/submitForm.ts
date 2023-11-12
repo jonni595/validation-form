538a571c2cdd301997a637231e2e9b2bb7c01326
Jonathan <jonathandajome@gmail.com>
2023-11-12 15:16:48 -0500

    optimized submitForm function

--- src/utils/submitForm.ts
+++ src/utils/submitForm.ts
@@ -1,36 +1,51 @@
 import { PersonProps } from "./model";
 
 export const submitForm = (person: PersonProps) => {
-  const regexExpressions = {
-    user: /^[a-zA-Z0-9_-]{4,16}$/,
-    name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
-    password: /^.{4,12}$/,
-    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    phone: /^\d{7,14}$/,
+  const validations = {
+    user: {
+      regex: /^[a-zA-Z0-9_-]{4,16}$/,
+      error: "El nombre de usuario debe tener entre 4 y 16 caracteres",
+    },
+    firstName: {
+      regex: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+      error: "El nombre debe tener entre 1 y 40 caracteres",
+    },
+    password: {
+      regex: /^.{4,12}$/,
+      error: "La contraseña debe tener entre 4 y 12 caracteres",
+    },
+    password2: {
+      regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,12}$/,
+      error:
+        "La contraseña debe tener al menos una letra minúscula, una letra mayúscula y un número",
+    },
+    email: {
+      regex: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+      error: "La dirección de correo electrónico no tiene un formato válido",
+    },
+    phone: {
+      regex: /^\d{7,14}$/,
+      error: "El número de teléfono no tiene un formato válido",
+    },
+    accept: {
+      regex: /\btrue\b/,
+      error: "Debes aceptar los términos de servicio",
+    },
   };
 
   return new Promise<void>((resolve, reject) => {
     setTimeout(() => {
-      /* if (person.user.length === 0 && !regexExpressions.user.test(person.user)) {
-        reject(
-          new Error(
-            "please, complete the fields or Letras, numeros, guion y guion_bajo"
-          )
-        );
+      const errors = [];
+      for (const [field, validation] of Object.entries(validations)) {
+        if (!validation.regex.test(person[field])) {
+          errors.push(validation.error);
+        }
+      }
+
+      if (errors.length > 0) {
+        reject(new Error(errors.join("\n")));
       } else {
         resolve();
-      } */
-
-      if (!regexExpressions.user.test(person.user)) {
-        reject(new Error("must be 4 digits"));
-      } else if (!regexExpressions.name.test(person.firstName)) {
-        reject(new Error("must be 40 digits"));
-      } else if (!regexExpressions.password.test(person.password)) {
-        reject(new Error("must be 12 digits"));
-      } else if (!regexExpressions.email.test(person.email)) {
-        reject(new Error("write the email correctly"));
-      } else if (!regexExpressions.phone.test(person.phone)) {
-        reject(new Error("must be 12 digits"));
       }
     }, 1500);
   });
